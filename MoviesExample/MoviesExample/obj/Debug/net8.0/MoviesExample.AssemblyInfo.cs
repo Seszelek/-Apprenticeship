c820083d0d7351bdae957e86d0faf3cf2f5f11a3
Asia <asiatlalka@gmail.com>
2024-04-23 20:01:22 +0200

    Update tests

--- MoviesExample/MoviesExample/obj/Debug/net8.0/MoviesExample.AssemblyInfo.cs
+++ MoviesExample/MoviesExample/obj/Debug/net8.0/MoviesExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b45f0cade48f4523c110cf4d35380cdab115f25e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+858e9e8b6895517d645188ab8d77688ae02ee298")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MoviesExample/TestMovieExample/UnitTest1.cs
+++ MoviesExample/TestMovieExample/UnitTest1.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
 namespace TestMovieExample;
+
 using MoviesExample.Repo;
 using MoviesExample.Models;
 using MoviesExample.Controllers;
@@ -6,17 +10,90 @@ using MoviesExample.Controllers;
 public class Tests
 {
     private MoviesRepo _moviesRepo;
-    
+    // private MoviesController _controller;
+
+
     [SetUp]
     public void Setup()
     {
         _moviesRepo = new MoviesRepo();
+        // _controller = new MoviesController(_moviesRepo);
     }
 
+
     [Test]
-    public void TestCheckIfThisFilmIsInBase()
+    public void IsMovieInDatabase_ExistingMovie_ReturnsTrue()
     {
         Assert.IsTrue(_moviesRepo.IsMovieInDatabase("The Green Mile"));
     }
-    
+
+    [Test]
+    public void AddNewMovie_AddsMovieToDatabase()
+    {
+        // Arrange
+        Movie newMovie = new Movie("The Godfather", "Drama", "Francis Ford Coppola", 1972);
+        // Act
+        _moviesRepo.AddNewMovie(newMovie);
+        // Assert
+        Assert.That(_moviesRepo.movies.Any(x => x.Title == "The Godfather"), Is.True);
+    }
+
+    [Test]
+    public void GetMovies_ReturnsListOfMovies()
+    {
+        // Act
+        var result = _moviesRepo.GetMovies();
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<System.Collections.Generic.List<Movie>>(result);
+            Assert.That(result.Count(), Is.EqualTo(3));
+        });
+    }
+
+    [Test]
+    public void ShowLastAddedMovie_ReturnsLastAddedMovie()
+    {
+        // Act
+        var result = _moviesRepo.ShowLastAddedMovie();
+        var lastMovie = _moviesRepo.movies.Last();
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.IsNotNull(result);
+            Assert.That(result, Is.EqualTo(lastMovie));
+        });
+    }
+
+    [Test]
+    public void GetMovieGenre_ReturnsMoviesFromGivenGenre()
+    {
+        // Act
+        var result = _moviesRepo.GetMovieGenre("Science Fiction").ToList();
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.IsNotNull(result);
+            Assert.That(result.Count(), Is.EqualTo(1));
+            Assert.That(result.Any(x => x.Title == "Avatar"), Is.True);
+        });
+    }
+
+    [Test]
+    public void GetMovieYear_ReturnsOkResult()
+    {
+        // Act
+        var result = _moviesRepo.GetMovieYear(2009);
+        var resultV2 = _moviesRepo.GetMovieYear(2889);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Any(), Is.True);
+            Assert.That(resultV2.Any(), Is.False);
+        });
+    }
 }
